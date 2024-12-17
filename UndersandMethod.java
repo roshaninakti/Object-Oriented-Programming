81517098f5af5c78c61d12259420d94f42efd4ff
Roshani Nakti <109640992+roshaninakti@users.noreply.github.com>
2024-12-17 10:23:15 +0530

    Add files via upload

---
+++ UndersandMethod.java
@@ -0,0 +1,20 @@
+public  class UndersandMethod{
+
+  // no params no return value 
+          void m1(){
+            System.out.println("Hello solution provider....");
+  }
+ // no params return value
+   String m2(){
+           return "Hello Solution Provider....." ;
+ }
+ // takes params no return value 
+   void m3(String job_Title){
+    System.out.println("Hello solotion Provider " +  job_Title );
+ }
+// takes params  rerurn value 
+ String m4(String  jobtitle){
+          return "Hello Solution Provider " + jobtitle;
+ }
+
+}
---
+++ UndersandMethodMain.java
@@ -0,0 +1,16 @@
+public class UndersandMethodMain {
+ public static void main(String[] args) {
+          UndersandMethod  um = new UndersandMethod();
+          um.m1();
+
+       String str = um.m2();
+       System.out.println(str);
+
+        um.m3("Software engineer....");
+
+       String str1= um.m4(" Software Engineer");
+       System.out.println(str1);
+          
+ }
+          
+}
