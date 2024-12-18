6977b0ceefc732bc0b3a2d8ad2b827b4a8fc45a9
Roshani Nakti <109640992+roshaninakti@users.noreply.github.com>
2024-12-18 10:08:00 +0530

    Add files via upload

---
+++ PolymorphismMethod.java
@@ -0,0 +1,20 @@
+package oops;
+public class PolymorphismMethod {
+          int a = 10;
+          int b =20;
+          void sum(){
+                    System.out.println(a+b);
+          }
+          void sum(int x, int y){
+                    System.out.println(x+y);
+          }
+          void sum(double x, int y){
+                    System.out.println(x+y);
+          }
+          void sum(int a, int b, int c){
+                    System.out.println(a+b+c);
+          }
+          
+
+          
+}
---
+++ PolymorphismMethodMain.java
@@ -0,0 +1,12 @@
+package oops;
+
+public class PolymorphismMethodMain {
+          public static void main(String[] args) {
+                    PolymorphismMethod pm = new PolymorphismMethod();
+                    pm.sum();
+                    pm.sum(20, 90);
+                    pm.sum(2.9, 29);
+                    pm.sum(10, 30, 90);
+          }
+          
+}
